d674c1c98334e63438d3b15b0d937f7210e62355
TaherArfa <taherarfaoui@gmail.com>
2020-12-01 20:13:50 +0100

    first commit

---
+++ models/Person.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PersonSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  email: String,
+  phone: String,
+});
+
+module.exports = Person = mongoose.model("Person", PersonSchema);
